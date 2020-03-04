3ac052762ff65a50b9d7a2fd8f0511deb97474d1
Anna Dybas <a.m.dybas@gmail.com>
2020-03-04 12:03:30 -0500

    added links in footer to github and linkedIn

--- game-up-frontend/src/components/Footer.js
+++ game-up-frontend/src/components/Footer.js
@@ -2,14 +2,19 @@ import React from 'react'
 import NavbarBrand from 'react-bootstrap/NavbarBrand'
 import Navbar from 'react-bootstrap/Navbar'
 import Container from 'react-bootstrap/Container'
+import { FaLinkedin, FaGithub } from "react-icons/fa"
+
+
 const Footer =props => {
     return (
-        <div className="fixed-bottom">
-            <Navbar color="dark" >
-                <Container>
-                    <NavbarBrand>Footer</NavbarBrand>
-                </Container>
-            </Navbar>
+        <div className="bottom">
+            
+                <Navbar sticky="bottom" expand="lg" variant="dark" bg="dark">
+                    <Navbar.Brand target="_blank" href="https://www.linkedin.com/in/anna-dybas-5221519/">Anna Dybas < FaLinkedin /> </Navbar.Brand>
+                    <Navbar.Brand target="_blank" href="https://github.com/Mcheyo/Game-up-Frontend">Github Repo <FaGithub /> </Navbar.Brand>
+                    <Navbar.Brand target="_blank" href="https://www.linkedin.com/in/mansour-cheyo/">Mansour Cheyo < FaLinkedin /> </Navbar.Brand>   
+                </Navbar>
+            
         </div>
     )
  } 
